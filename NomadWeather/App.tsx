495d3d441f6c21d86c7b5007efa2ff940e0c919c
Junsu97 <wnstn7759@naver.com>
2024-11-06 15:07:55 +0900

    Chapter1

---
+++ NomadWeather/App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 28,
+    color: "red",
+  }
+});
+
+//! 2.2 까지 진행함
